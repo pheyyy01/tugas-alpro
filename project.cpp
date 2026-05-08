4cf7bd64aae88919b067da3f6e67e3a0ea737d74
pheyyy01 <sopamaul186@gmail.com>
2026-05-08 19:39:07 +0700

    menambahkan do while dan struck akan di tambah jika melanjutkan pembelian
    ph

--- project.cpp
+++ project.cpp
@@ -3,85 +3,106 @@ using namespace std;
 
 class Bakery {
 private:
-    int pilihan;
+    int pilihan, jumlah, hargaSatuan, totalHarga;
+    int totalSemua = 0;
     string namaRoti;
-    int jumlah;
-    int hargaSatuan;
-    int totalHarga;
+    char lagi;
 
 public:
+
+    void menu() {
+        cout << "\n===== TOKO BAKERY =====" << endl;
+        cout << "1. Roti Keju   = 12000" << endl;
+        cout << "2. Roti Tawar  = 9000" << endl;
+        cout << "3. Roti Coklat = 11000" << endl;
+        cout << "4. Roti Pisang = 10000" << endl;
+        cout << "0. Keluar" << endl;
+    }
+
     // INPUT
     void input() {
-        cout << "\n=== TOKO BAKERY ===" << endl;
-        cout << "Pilih roti:" << endl;
-        cout << "1. Roti Keju   (12000)" << endl;
-        cout << "2. Roti Tawar  (9000)" << endl;
-        cout << "3. Roti Coklat (11000)" << endl;
-        cout << "4. Roti Pisang (10000)" << endl;
-        cout << "0. Keluar" << endl;
-        cout << "Masukkan pilihan: ";
+        cout << "\nPilih Roti : ";
         cin >> pilihan;
 
         if (pilihan != 0) {
-            cout << "Masukkan jumlah beli: ";
+            cout << "Jumlah Beli : ";
             cin >> jumlah;
         }
     }
 
     // PROSES
     bool proses() {
+
         switch (pilihan) {
+
             case 1:
                 namaRoti = "Roti Keju";
                 hargaSatuan = 12000;
                 break;
+
             case 2:
                 namaRoti = "Roti Tawar";
                 hargaSatuan = 9000;
                 break;
+
             case 3:
                 namaRoti = "Roti Coklat";
                 hargaSatuan = 11000;
                 break;
+
             case 4:
                 namaRoti = "Roti Pisang";
                 hargaSatuan = 10000;
                 break;
+
             case 0:
-                cout << "\nTerima kasih, program selesai ??" << endl;
-                return false; // keluar dari program
+                cout << "\nTerima kasih sudah berbelanja!" << endl;
+                return false;
+
             default:
-                cout << "Pilihan tidak valid!" << endl;
+                cout << "\nPilihan tidak tersedia!" << endl;
                 return true;
         }
 
         totalHarga = jumlah * hargaSatuan;
+
+        // MENAMBAHKAN TOTAL SEMUA BELANJA
+        totalSemua += totalHarga;
+
         return true;
     }
 
     // OUTPUT
     void output() {
-        if (pilihan != 0 && hargaSatuan != 0) {
-            cout << "\n=== STRUK PEMBELIAN ===" << endl;
-            cout << "Nama Roti     : " << namaRoti << endl;
-            cout << "Jumlah        : " << jumlah << endl;
-            cout << "Harga Satuan  : " << hargaSatuan << endl;
-            cout << "Total Harga   : " << totalHarga << endl;
-            
-            cout<<"\n========================="<<endl;
+
+        if (pilihan != 0) {
+
+            cout << "\n===== STRUK PEMBELIAN =====" << endl;
+            cout << "Nama Roti    : " << namaRoti << endl;
+            cout << "Jumlah Beli  : " << jumlah << endl;
+            cout << "Harga Satuan : " << hargaSatuan << endl;
+            cout << "Total Harga  : " << totalHarga << endl;
+
+            // TOTAL KESELURUHAN
+            cout << "---------------------------" << endl;
+            cout << "Total Semua  : " << totalSemua << endl;
         }
     }
 };
 
 int main() {
+
     Bakery toko;
     bool jalan = true;
 
+    // DO WHILE
     do {
+
+        toko.menu();
         toko.input();
         jalan = toko.proses();
         toko.output();
+
     } while (jalan);
 
     return 0;
-}
